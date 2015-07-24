69842f7b2aea386f74302f8635e23b16dc7e6b47
Damir Perisic <underscope@gmail.com>
2015-07-24 16:41:36 +0200

    Bootstrap competencies

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,8 @@
 
 Meteor.startup(function() {
   Competencies.remove({});
+  Competencies.insert({ name: 'Competency A' });
+  Competencies.insert({ name: 'Competency B' });
+  Competencies.insert({ name: 'Competency C' });
+  Competencies.insert({ name: 'Competency D' });
 });
