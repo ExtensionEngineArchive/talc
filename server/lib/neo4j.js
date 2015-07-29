23b9a08e174956b5d98532509649b572de9adc6b
Damir Perisic <underscope@gmail.com>
2015-07-29 11:21:31 +0200

    neo4j cleanup

--- server/lib/neo4j.js
+++
@@ -1,6 +0,0 @@
-
-Meteor.neo4j.connectionURL = 'http://neo4j:temp123@localhost:7474';
-Meteor.neo4j.allowClientQuery = true;
-
-// Deny all writing actions on client
-Meteor.neo4j.set.deny(Meteor.neo4j.rules.write);
