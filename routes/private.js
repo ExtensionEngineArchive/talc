5cd05df3bfcf228172189724cac7577c4b044aed
Damir Perisic <underscope@gmail.com>
2015-08-03 00:42:07 +0200

    Competency editor route update

--- routes/private.js
+++ routes/private.js
@@ -1,26 +1,16 @@
-
 Router.route('/:_id/competency-editor', function() {
   this.render('competencyEditor', {
     data: function() {
       var competency = Competencies.findOne({ _id: this.params._id });
-      var browser = Session.get('ceBrowser') || {};
+      if (competency) {
+        var browserService = Dependency.get('browserService');
+        browserService.init(competency);
 
-      if (!browser.path || (browser.path[0].id !== competency._id)) {
-        browser.path = [];
-        browser.path.push({
-          id: competency._id,
-          name: competency.name,
-          type: 'C'
-        });
+        return {
+          competency: competency,
+          graph: cytoscape({ elements: competency.elements })
+        };
       }
-
-      Session.set('ceCompetency', competency);
-      Session.set('ceBrowser', browser);
-
-      return {
-        competency: competency,
-        graph: cytoscape({ elements: competency.elements })
-      };
     }
   });
 }, {
