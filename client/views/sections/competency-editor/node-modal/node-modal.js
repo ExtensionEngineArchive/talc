6260f7682e27f03ddc4d578b7d15dc95543af6b8
Damir Perisic <underscope@gmail.com>
2015-08-03 12:17:39 +0200

    Create node modal outline

---
+++ client/views/sections/competency-editor/node-modal/node-modal.js
@@ -0,0 +1,13 @@
+
+Template.ceNodeModal.onCreated(function() {
+  this.tempStorage = {
+    parents: [],
+    prerequisites: []
+  };
+});
+
+Template.ceNodeModal.helpers({
+});
+
+Template.ceNodeModal.events({
+});
