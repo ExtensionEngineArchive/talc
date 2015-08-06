f7f1c9f478b3064bfd1af2e921327ab190da26d3
Damir Perisic <underscope@gmail.com>
2015-08-06 22:54:39 +0200

    Competency editor menu update

--- client/views/sections/competency-editor/menu/menu.js
+++ client/views/sections/competency-editor/menu/menu.js
@@ -1,9 +1,18 @@
 
+var competencyService;
+
+Dependency.autorun(function() {
+  competencyService = Dependency.get('competencyService');
+});
+
 Template.ceMenu.events({
   'click #createNode': function() {
     $('#ceNodeModal').modal('show');
   },
   'click #showGraph': function() {
-    $('#ceGraphModal').modal('show');
+    competencyService.editor.view.switchToGraph();
+  },
+  'click #showList': function() {
+    competencyService.editor.view.switchToList();
   }
 });
