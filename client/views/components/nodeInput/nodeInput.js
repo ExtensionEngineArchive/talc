dae26441c035b7a66b6e037878b0cc514fb537ac
aborzic@extensionengine.com <aborzic@extensionengine.com>
2015-08-06 16:08:29 +0200

    Revert "Enable adding parent skill without the '+' sign"
    This reverts commit 480cbf53b1eda7b2cd7dfe543d3e1b71400d1093.

--- client/views/components/nodeInput/nodeInput.js
+++ client/views/components/nodeInput/nodeInput.js
@@ -27,13 +27,17 @@ Template.nodeInputComponent.events({
     var result = competencyService.nodes.findAllByName(t.$('.search').val(), 20);
     Template.instance().searchResult.set(result);
   },
+  'focusout .search': function() {
+    Template.instance().searchResult.set([]);
+    Template.instance().searchDisplay.set(false);
+  },
   'click .show-input': function(e, t) {
     Template.instance().searchDisplay.set(true);
     setTimeout(function() {
       t.$('.search').focus();
     }, 100);
   },
-  'click .list-group-item': function(e, t) {
+  'click .add': function(e, t) {
     var selected = t.data.selected.get();
     selected.push(this);
     t.data.selected.set(selected);
