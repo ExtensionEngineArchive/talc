281bfbf8df15d0c72ac32ef9f4fa1047a7ceadd0
Damir Perisic <underscope@gmail.com>
2015-08-07 09:55:02 +0200

    Services update

--- client/services/competency-service.js
+++ client/services/competency-service.js
@@ -7,7 +7,6 @@ Dependency.autorun(function() {
 
 Dependency.add('competencyService', (function competencyService() {
   var s = {
-    context: {},
     nodes: {}
   };
 
@@ -16,23 +15,44 @@ Dependency.add('competencyService', (function competencyService() {
     graph: new ReactiveVar('')
   };
 
+  /**
+   * @summary Initialize service context and other dependencies
+   * @method init
+   * @param {Object} [competency] Competency node
+   */
   s.init = function(competency) {
     data.competency.set(competency);
-    data.graph.set(cytoscape({ elements: competency.elements }));
+    data.graph.set(cytoscape({ headless: true, elements: competency.elements }));
     browserService.init(data.competency.get());
   };
 
-  s.context.competency = function() {
+  /**
+   * @summary Get competency
+   * @method competency
+   * @returns {Object}
+   */
+  s.competency = function() {
     return data.competency.get();
   };
 
-  s.context.graph = function() {
+  /**
+   * @summary Get headless graph instance
+   * @method graph
+   * @returns {Object}
+   */
+  s.graph = function() {
     return data.graph.get();
   };
 
+  /**
+   * @summary Get node ids
+   * @method nodes.ids
+   * @param {Object} [graph] Cytoscape instance, optional, context is used otherwise
+   * @returns {Array}
+   */
   s.nodes.ids = function(graph) {
     var result = [];
-    graph = graph || s.context.graph();
+    graph = graph || s.graph();
 
     graph.nodes().forEach(function(it) {
       result.push(it.id());
@@ -41,12 +61,18 @@ Dependency.add('competencyService', (function competencyService() {
     return result;
   };
 
+  /**
+   * @summary Add node
+   * @method nodes.add
+   * @param {Object} [node] Node data
+   * @param {Array} [parents] Parent nodes
+   */
   s.nodes.add = function(node, parents) {
-    parents = parents || [s.context.competency()];
+    parents = parents || [s.competency()];
 
     Meteor.call('nodes.insert', node, function(error, _id) {
       var params = {
-        competency: s.context.competency(),
+        competency: s.competency(),
         node: Nodes.findOne({ _id: _id }),
         parents: parents
       };
@@ -55,13 +81,22 @@ Dependency.add('competencyService', (function competencyService() {
     });
   };
 
+  /**
+   * @summary Filter nodes by name
+   * @method nodes.findAllByName
+   * @param {String} [search] Search string
+   * @param {Number} [limit] Max number of results, 30 default
+   * @returns {Array}
+   */
   s.nodes.findAllByName = function(search, limit) {
     limit = limit || 30;
     return Nodes.find({
       $and: [
         { _id: { $in: s.nodes.ids() } },
-        { name: new RegExp(search) }
-      ]}, { limit: limit }).fetch();
+        { name: new RegExp('^' + search.toLowerCase(), 'i') }
+      ]
+    },
+    { limit: limit }).fetch();
   };
 
   return s;
