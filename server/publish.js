9988b4e01e8852c452dd9f770362bc5663ca2af4
Damir Perisic <underscope@gmail.com>
2015-08-20 21:25:32 +0200

    Publishing graph data based on role

--- server/publish.js
+++ server/publish.js
@@ -1,29 +1,20 @@
 
 Meteor.publish("knowledgeGraphs", function() {
-  var user = Meteor.users.findOne(this.userId);
-  if (user) {
-    if (user.profile.admin) {
-      return Nodes.find({ type: 'C' });
-    }
-
-    return Nodes.find({
-      $and: [
-        { type: 'C' },
-        { users: { $elemMatch: { _id: this.userId } } }
-      ]
-    });
+  if (this.userId) {
+    var user = Meteor.users.findOne(this.userId);
+    return TALCH.user.graphs(user);
+  } else {
+    this.ready();
   }
 });
 
 Meteor.publish("knowledgeGraphNodes", function(_id) {
-  var user = Meteor.users.findOne(this.userId);
-  if (user) {
-    var root = Nodes.findOne({ _id: _id });
-    if (root) {
-      if (user.profile.admin || (Lazy(root.users).findWhere({ _id: user._id }))) {
-        var graph = cytoscape({ headless: true, elements: root.elements });
-        return Nodes.find({ _id: { $in: Nodes.graph.ids(graph) } });
-      }
+  if (this.userId) {
+    var user = Meteor.users.findOne(this.userId);
+    if (TALCH.graph.hasAccess(user, _id)) {
+      return TALCH.graph.nodes(_id);
     }
+  } else {
+    this.ready();
   }
 });
