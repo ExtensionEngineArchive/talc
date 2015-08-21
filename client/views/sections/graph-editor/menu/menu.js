d91b85d13cc92b46f785ca13ffe0f23e434b9899
Damir Perisic <underscope@gmail.com>
2015-08-21 14:01:08 +0200

    Access settings from left nav

--- client/views/sections/graph-editor/menu/menu.js
+++ client/views/sections/graph-editor/menu/menu.js
@@ -5,6 +5,15 @@ Dependency.autorun(function() {
   editorService = Dependency.get('editorService');
 });
 
+Template.geMenu.helpers({
+  graph: function() {
+    return editorService.context.root();
+  },
+  userIsAdmin: function() {
+    return TALCH.user.isGraphAdmin(Meteor.user(), editorService.context.root()._id);
+  }
+});
+
 Template.geMenu.events({
   'click #createNode': function() {
     $('#ceNodeModal').modal('show');
