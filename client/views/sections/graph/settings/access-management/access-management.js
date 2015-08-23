35e981e127e360c9578cce07f453e8023c9f4f5a
Damir Perisic <underscope@gmail.com>
2015-08-23 18:48:14 +0200

    Access management template update

--- client/views/sections/graph/settings/access-management/access-management.js
+++ client/views/sections/graph/settings/access-management/access-management.js
@@ -2,6 +2,18 @@
 Template.gsAccessManagement.helpers({
   members: function() {
     return TALCH.graph.users(this.graphRoot._id);
+  },
+  autocompleteEmail: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: '',
+        collection: Meteor.users,
+        field: "emails.0.address",
+        template: Template.autocompleteUser
+      }]
+    };
   }
 });
 
