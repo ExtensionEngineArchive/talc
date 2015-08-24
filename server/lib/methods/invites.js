fa6085cbc666dfe31793587fcfb53392d079a3da
Damir Perisic <underscope@gmail.com>
2015-08-24 17:32:23 +0200

    Invites update

--- server/lib/methods/invites.js
+++ server/lib/methods/invites.js
@@ -1,14 +1,30 @@
 
 Meteor.methods({
-  // TODO: Validation, resend
-  'invites.send': function(email) {
-    var existingUser = Meteor.users.findOne({ email: email });
-    if (!existingUser) {
-      var userId = Accounts.createUser({ email: email, password: Random.hexString(10) });
-      Accounts.sendEnrollmentEmail(userId);
-      return userId;
-    } else {
-      return existingUser._id;
+  'invites.graph': function(email, graphId, role) {
+    if (!Meteor.userId() || !TALCH.user.isGraphAdmin(Meteor.user(), graphId)) {
+      throw new Meteor.Error("not-authorized");
     }
+
+    if (!TALCH.graph.isValidRole(role)) {
+      throw new Meteor.Error("Invalid role");
+    }
+
+    var user = Meteor.users.findOne({ emails: { $elemMatch: { address: email } } });
+    if (!user) {
+      user = createUser(email);
+    }
+
+    Roles.addUsersToRoles(user._id, [role], graphId);
   }
 });
+
+function createUser(email) {
+  var _id = Accounts.createUser({ email: email, password: Random.hexString(10) });
+  var user = Meteor.users.findOne({ _id: _id });
+
+  Meteor.setTimeout(function () {
+    Accounts.sendEnrollmentEmail(_id);
+  });
+
+  return user;
+}
