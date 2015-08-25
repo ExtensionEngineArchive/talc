5d412ecd0822bc27e59ea4ccb61c754b90c2387e
Damir Perisic <underscope@gmail.com>
2015-08-25 16:26:33 +0200

    Graph editor menu update.
    - Use editor service for triggering node modal

--- client/views/sections/graph/editor/menu/menu.js
+++ client/views/sections/graph/editor/menu/menu.js
@@ -1,27 +1,27 @@
 
-var editorService;
+var editor;
 
 Dependency.autorun(function() {
-  editorService = Dependency.get('editorService');
+  editor = Dependency.get('editorService');
 });
 
 Template.geMenu.helpers({
   graph: function() {
-    return editorService.context.root();
+    return editor.context.root();
   },
   userIsAdmin: function() {
-    return TALCH.user.isGraphAdmin(Meteor.user(), editorService.context.root()._id);
+    return TALCH.user.isGraphAdmin(Meteor.user(), editor.context.root()._id);
   }
 });
 
 Template.geMenu.events({
   'click #createNode': function() {
-    $('#ceNodeModal').modal('show');
+    editor.modals.edit.new();
   },
   'click #showGraph': function() {
-    editorService.view.switchToGraph();
+    editor.view.switchToGraph();
   },
   'click #showList': function() {
-    editorService.view.switchToList();
+    editor.view.switchToList();
   }
 });
