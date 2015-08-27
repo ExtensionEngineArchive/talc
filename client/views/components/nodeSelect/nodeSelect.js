263a8cc96d9993b77b1c67fd7d4d604d2992e585
Damir Perisic <underscope@gmail.com>
2015-08-27 12:50:13 +0200

    nodeSelectComponent init

---
+++ client/views/components/nodeSelect/nodeSelect.js
@@ -0,0 +1,34 @@
+
+var editor;
+
+Dependency.autorun(function() {
+  editor = Dependency.get('editorService');
+});
+
+Template.nodeSelectComponent.onCreated(function() {
+  this.searchResult = new ReactiveVar([]);
+});
+
+Template.nodeSelectComponent.helpers({
+  searchResult: function() {
+    return Template.instance().searchResult.get();
+  }
+});
+
+Template.nodeSelectComponent.events({
+  'input .search': function(e, t) {
+    var result = editor.nodes.findAllByName(t.$('.search').val(), 20);
+    Template.instance().searchResult.set(result);
+  },
+  'blur .search': function(e, t) {
+    Template.instance().searchResult.set([]);
+  },
+  'focus .search': function(e, t) {
+    Template.instance().searchResult.set(editor.nodes.findAllByName('', 20));
+  },
+  'mousedown .add': function(e, t) {
+    t.data.selected.set(this);
+    t.$('.search').val('');
+    Template.instance().searchResult.set([]);
+  }
+});
