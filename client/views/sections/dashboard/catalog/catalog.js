b1c0cf8d43a8a486becf0703e0bea6c6bdc0f055
Damir Perisic <underscope@gmail.com>
2015-08-28 09:07:01 +0200

    Catalog search

--- client/views/sections/dashboard/catalog/catalog.js
+++ client/views/sections/dashboard/catalog/catalog.js
@@ -1,6 +1,24 @@
 
+Template.dCatalog.onCreated(function() {
+  this.search = new ReactiveVar('');
+});
+
 Template.dCatalog.helpers({
   knowledgeGraphs: function() {
-    return Nodes.find({ type: 'R' });
+    if (Template.instance().search.get()) {
+      return Nodes.find({
+        $and: [
+          { type: 'R' },
+          { name: new RegExp('^' + Template.instance().search.get().toLowerCase(), 'i') }
+        ]});
+    } else {
+      return Nodes.find({ type: 'R' });
+    }
+  }
+});
+
+Template.dCatalog.events({
+  'input #catalogSearch': function(e, t) {
+    Template.instance().search.set(t.$('#catalogSearch').val());
   }
 });
