7557bdac0d4c63caff54f7468277428a40b01ad0
Damir Perisic <underscope@gmail.com>
2015-08-29 09:40:29 +0200

    Graph editor update
    - Outline view added

--- client/views/sections/graph/editor/graph-editor.js
+++ client/views/sections/graph/editor/graph-editor.js
@@ -6,8 +6,17 @@ Dependency.autorun(function() {
 });
 
 Template.graphEditor.helpers({
+  root: function() {
+    return editor.context.root();
+  },
   'listView': function() {
     return editor.view.isList();
+  },
+  'graphView': function() {
+    return editor.view.isGraph();
+  },
+  'outlineView': function() {
+    return editor.view.isOutline();
   },
   'renderNodeModal': function() {
     return editor.modals.edit.rendered();
