cae5e312b6aaeb5879997fb472c237ac53e6b734
Damir Perisic <underscope@gmail.com>
2015-08-30 18:46:53 +0200

    Routes update
    - Enrol account route added
    - Reset password route added

--- routes/public.js
+++ routes/public.js
@@ -4,11 +4,23 @@ Router.plugin('auth', {
     route: 'signIn'
   },
   except: [
-    'signIn'
+    'signIn',
+    'enrollAccount',
+    'resetPassword'
   ]
 });
 
 Router.route('/sign-in', {
   name: 'signIn',
+  layoutTemplate: 'AuthLayout'
+});
+
+Router.route('/enroll-account/:token', {
+  name: 'enrollAccount',
+  layoutTemplate: 'AuthLayout'
+});
+
+Router.route('/reset-password/:token', {
+  name: 'resetPassword',
   layoutTemplate: 'AuthLayout'
 });
