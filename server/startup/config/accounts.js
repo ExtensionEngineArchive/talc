af81177027d376c0a32f0c55a133f9c4b5164318
Damir Perisic <underscope@gmail.com>
2015-08-30 18:31:25 +0200

    Accounts config update
    - Set urls for password reset and enrollment

--- server/startup/config/accounts.js
+++ server/startup/config/accounts.js
@@ -1,4 +1,12 @@
 
 Meteor.startup(function() {
   Accounts.emailTemplates.from = 'TALC <' + Meteor.settings.private.email.address + '>';
+
+  Accounts.urls.enrollAccount = function(token) {
+    return Meteor.absoluteUrl('enroll-account/' + token);
+  };
+
+  Accounts.urls.resetPassword = function(token) {
+    return Meteor.absoluteUrl('reset-password/' + token);
+  };
 });
