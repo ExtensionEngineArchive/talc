b0b734b8460621e8a9a4cfbe3110cf1b9efc0b37
Damir Perisic <underscope@gmail.com>
2015-09-02 09:26:03 +0200

    Graph legend update

--- client/views/components/graphLegend/graphLegend.js
+++ client/views/components/graphLegend/graphLegend.js
@@ -1,14 +1,15 @@
+
 Template.graphLegendComponent.helpers({
   items: function () {
     var results = [];
-
     Lazy(Nodes.TYPE.list).each(function (it) {
       results.push({
-        type: it,
-        name: Nodes.TYPE[it].name
+        code: Nodes.TYPE[it].code,
+        name: Nodes.TYPE[it].name,
+        color: Nodes.TYPE[it].color
       });
     });
-    
+
     return results;
   }
 });
