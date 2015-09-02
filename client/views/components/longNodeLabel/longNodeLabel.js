74bd06595aeba96735201a7446c48cf7c2a41a88
Damir Perisic <underscope@gmail.com>
2015-09-02 09:06:35 +0200

    Cleanup

--- client/views/components/longNodeLabel/longNodeLabel.js
+++
@@ -1,8 +0,0 @@
-Template.longNodeLabelComponent.helpers({
-  color: function() {
-    return Nodes.TYPE[this.node.type].color;
-  },
-  fullName: function () {
-    return Nodes.TYPE[this.node.type].name;
-  }
-});
--- client/views/components/shortNodeLabel/shortNodeLabel.js
+++
@@ -1,8 +0,0 @@
-
-Template.shortNodeLabelComponent.helpers({
-  color: function() {
-    if (this.node) {
-      return Nodes.TYPE[this.node.type].color;
-    }
-  }
-});
