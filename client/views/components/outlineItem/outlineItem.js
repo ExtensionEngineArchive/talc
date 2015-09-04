500b6381729c7f8bf2d03f6fb92dc9375e44211c
Damir Perisic <underscope@gmail.com>
2015-09-04 09:36:19 +0200

    Outline item update
    - Quick Contextual Add

--- client/views/components/outlineItem/outlineItem.js
+++ client/views/components/outlineItem/outlineItem.js
@@ -5,6 +5,10 @@ Dependency.autorun(function() {
   editor = Dependency.get('editorService');
 });
 
+Template.outlineItemComponent.onCreated(function() {
+  this.showNodeInput = new ReactiveVar(false);
+});
+
 Template.outlineItemComponent.helpers({
   root: function() {
     return Template.instance().data.root;
@@ -12,11 +16,6 @@ Template.outlineItemComponent.helpers({
   children: function() {
     var children = editor.nodes.children(Template.instance().data.root._id).fetch();
     return children.length > 0 ? children : null;
-  },
-  expand: function() {
-    var itemRoot = Template.instance().data.root;
-    var graphRoot = editor.context.root();
-    return itemRoot._id == graphRoot._id ? 'in' : '';
   },
   'nodeType': function(node) {
     return Nodes.TYPE[node.type].code;
@@ -26,6 +25,33 @@ Template.outlineItemComponent.helpers({
   },
   nodeNumber: function(node) {
     return editor.nodes.number(node._id);
+  },
+  childTypes: function(node) {
+    var results = [];
+    var types = [];
+
+    if ((node.type == 'R') || (node.type == 'T')) {
+      types = ['T', 'O'];
+    } else if (node.type == 'O') {
+      types = ['S'];
+    } else {
+      return [];
+    }
+
+    Lazy(Nodes.TYPE.list).each(function (it) {
+      if (Lazy(types).contains(it)) {
+        results.push({
+          code: Nodes.TYPE[it].code,
+          name: Nodes.TYPE[it].name,
+          color: Nodes.TYPE[it].color
+        });
+      }
+    });
+
+    return results;
+  },
+  showNodeInput: function() {
+    return Template.instance().showNodeInput.get();
   }
 });
 
@@ -34,5 +60,21 @@ Template.outlineItemComponent.events({
     editor.select(this.root);
     t.$('.panel-heading').removeClass('node-selected');
     t.$('#heading' + this.root._id).addClass('node-selected');
+  },
+  'click .add-node': function(e, t) {
+    e.preventDefault();
+
+    var node = {
+      name: t.$('[name=name]').val(),
+      type: t.$('[name=type]').val()
+    };
+
+    editor.nodes.add(node, [this.root]);
+    Template.instance().showNodeInput.set(false);
+    return false;
+  },
+  'click .show-node-input': function() {
+    Template.instance().showNodeInput.set(true);
+    return false;
   }
 });
