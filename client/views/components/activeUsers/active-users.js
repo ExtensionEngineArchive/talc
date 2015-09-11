d2b0669e91f9375b7eebc3a5c098eaa37918c271
Damir Perisic <underscope@gmail.com>
2015-09-11 15:48:05 +0200

    Update activeUsersComponent

--- client/views/components/activeUsers/active-users.js
+++ client/views/components/activeUsers/active-users.js
@@ -5,6 +5,12 @@ Dependency.autorun(function() {
   activity = Dependency.get('activityService');
 });
 
+Template.activeUsersComponent.onRendered(function() {
+  $('.active-users-component ul').on('DOMNodeInserted', function() {
+    $(this).find('[data-toggle="tooltip"]').tooltip();
+  });
+});
+
 Template.activeUsersComponent.helpers({
   users: function() {
     return activity.users();
