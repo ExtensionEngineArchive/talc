7a709fd6ddbd60d3cb61a15b594620e54a172ea0
Damir Perisic <underscope@gmail.com>
2015-09-12 18:50:07 +0200

    Activity service update

--- client/services/activity-service.js
+++ client/services/activity-service.js
@@ -1,40 +1,70 @@
 
+// TODO: NTP
 Dependency.add('activityService', (function activityService() {
   var s = {};
 
   var data = {
-    graph: new ReactiveVar(null),
-    users: new ReactiveVar([]),
-    nodes: new ReactiveVar([]),
-    threshold: 30
+    graph: new ReactiveVar(null), // Graph being edited (root node)
+    users: new ReactiveVar([]),   // Other users that are editing the graph
+    nodes: new ReactiveVar([]),   // Nodes being edited (by others)
+    inactivityThreshold: 30,      // For detecting inactivity of other users, in seconds
+    reportStatusInterval: 20000,  // For reporting activity, in milliseconds (ping)
+    markerColors: [
+      '#EC407A',                  // Pink
+      '#3F51B5',                  // Indigo
+      '#9CCC65'                   // Green
+    ]
   };
 
+  /**
+   * @summary Start activity tracking
+   * @method start
+   * @param {Object} [graph] Graph (root) node
+   */
   s.start = function(graph) {
     if (data.graph.get() != graph) {
       data.graph.set(graph);
-      update();
+      updateStatus();
     }
   };
 
+  /**
+   * @summary Stop activity tracking
+   * @method stop
+   */
   s.stop = function() {
     data.graph.set(null);
   };
 
+  /**
+   * @summary Get active users (That are using graph editor)
+   * @method users
+   * @return {Array}
+   */
   s.users = function() {
     return data.users.get();
   };
 
-  function update() {
+  /**
+   * @summary Update user status (ping)
+   * @method updateStatus
+   */
+  function updateStatus() {
     Meteor.call('activity.graph', data.graph.get());
   }
 
-  function getUsers() {
+  /**
+   * @summary Retrieve active users
+   * @method getActiveUsers
+   * @return {Array}
+   */
+  function getActiveUsers() {
     var users = [];
     var activities = Redis.matching('user::*::graph::' + data.graph.get()).fetch();
 
     if (activities.length > 0) {
       var threshold = new Date();
-      threshold.setSeconds(threshold.getSeconds() - data.threshold);
+      threshold.setSeconds(threshold.getSeconds() - data.inactivityThreshold);
 
       Lazy(activities).each(function(it) {
         it.value = new Date(it.value);
@@ -50,24 +80,27 @@ Dependency.add('activityService', (function activityService() {
         $and: [
           { _id: { $in: users } },
           { 'status.online': true }
-        ]
-      }).fetch();
+        ]}).fetch();
     }
 
-    updateUsers(users);
+    return users;
   }
 
-  function updateUsers(newValue) {
-    var oldValue = Lazy(data.users.get()).pluck('_id');
-    var intersection = Lazy(Lazy(newValue).pluck('_id')).intersection(oldValue).toArray();
+  /**
+   * @summary Set active users
+   * @method setActiveUsers
+   * @param {Array} [newValue] Active users
+   */
+  function setActiveUsers(newValue) {
+    var oldValue = data.users.curValue;
 
-    var colors = [
-      '#F44236', '#EA1E63', '#9C28B1', '#673BB7', '#3F51B5',
-      '#FFEB3C', '#FF9700', '#FE5722', '#9E9E9E', '#607D8B'
-    ];
+    // Calculate diff
+    var oldValueIds = Lazy(oldValue).pluck('_id');
+    var newValueIds = Lazy(newValue).pluck('_id');
+    var intersection = Lazy(newValueIds).intersection(oldValueIds).toArray();
 
-    if (intersection.length != newValue.length) {
-      var result = [];
+    var result = [];
+    if ((intersection.length != newValue.length) || (newValue.length != oldValue.length)) {
       Lazy(oldValue).each(function(it) {
         if (Lazy(intersection).contains(it._id)) {
           result.push(it);
@@ -75,8 +108,12 @@ Dependency.add('activityService', (function activityService() {
       });
 
       Lazy(newValue).each(function(it) {
-        if (!Lazy(oldValue).contains(it._id)) {
-          result.push({ _id: it._id, profile: it.profile, color: colors[Math.floor(Math.random() * 10)] });
+        if (!Lazy(oldValueIds).contains(it._id)) {
+          result.push({
+            _id: it._id,
+            profile: it.profile,
+            color: data.markerColors[result.length % 3]
+          });
         }
       });
 
@@ -84,19 +121,19 @@ Dependency.add('activityService', (function activityService() {
     }
   }
 
-  // Track
+  // Track activities
   Tracker.autorun(function() {
     if (data.graph.get()) {
-      getUsers();
+      setActiveUsers(getActiveUsers());
     }
   });
 
-  // Ping
+  // Report status
   setInterval(function() {
     if (data.graph.get()) {
-      update();
+      updateStatus();
     }
-  }, 30000);
+  }, data.reportStatusInterval);
 
   return s;
 })());
